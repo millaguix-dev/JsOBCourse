b2cccf6ab3cb6f9e70550deba6486d67fee5098b
millaguix-dev <106613541+millaguix-dev@users.noreply.github.com>
2023-04-04 13:59:11 -0400

    Ejercicio 02

---
+++ Exercise02/index.js
@@ -0,0 +1,15 @@
+const nombre = "Carlos Millán";
+const edad = 21;
+const imDesarrollador = true;
+const fecha_nacimiento = new Date(2001,11,31);
+
+const libro_fav = {
+    titulo: "Oliver Twist",
+    autor: "Charles Dickens",
+    fecha: 1837,
+    url: "https://www.amazon.com/Charles-Dickens/dp/B09FS9NZQH/ref=sxin_16_pa_sp_search_thematic_sspa?__mk_es_US=ÅMÅŽÕÑ&content-id=amzn1.sym.d55a2bc9-0c1b-4157-be02-ff8f549e91d4%3Aamzn1.sym.d55a2bc9-0c1b-4157-be02-ff8f549e91d4&crid=1JK5ZR6L2OQNK&cv_ct_cx=oliver+twist+book&keywords=oliver+twist+book&pd_rd_i=B09FS9NZQH&pd_rd_r=c8fbe343-60ff-4ca7-bfa1-682f6bba2b4e&pd_rd_w=qyql7&pd_rd_wg=hBoc9&pf_rd_p=d55a2bc9-0c1b-4157-be02-ff8f549e91d4&pf_rd_r=P1ZSPS188K8HJFFADM6F&qid=1680630922&sbo=RZvfv%2F%2FHxDF%2BO5021pAnSA%3D%3D&sprefix=oliver+twist+bo%2Caps%2C296&sr=1-3-6caf8c80-d701-4184-90ff-f670949d61c2-spons&psc=1&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUE0T1Y4QVNPODQ1T08mZW5jcnlwdGVkSWQ9QTAzMzM0OTEzNTlIVEpSQUJCWFBSJmVuY3J5cHRlZEFkSWQ9QTAyODM4NjIyM05HTjZBNzM2WklVJndpZGdldE5hbWU9c3Bfc2VhcmNoX3RoZW1hdGljJmFjdGlvbj1jbGlja1JlZGlyZWN0JmRvTm90TG9nQ2xpY2s9dHJ1ZQ==",
+}
+
+const lista = [nombre,edad,imDesarrollador,fecha_nacimiento,libro_fav];
+
+console.log(lista)
